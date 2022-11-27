f5d95a6b99df5a33f19978f90e85d7d7291777c6
root <root@h2850726.stratoserver.net>
2022-11-27 11:49:25 +0100

    setup

---
+++ serverhealth.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+#ServerHealth.sh gathers infos about a server before rebooting it. Upon rerun the script will check if something is missing
+#Repository: https://github.com/daXenlo/serverhealth.sh 
+#
+
+
+###Variables###
+
+
+
+
+
+###Setup###
+
+
+
+
+###Output###
+
